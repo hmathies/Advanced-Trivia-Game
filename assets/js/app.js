04b124015db4e20465cce0c76573baa3d814d64d
Heather Mathies <heathermathies@Heathers-MacBook-Air.local>
2017-09-03 20:13:31 -0400

    formatted all code

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,19 @@
-//Built by Heather Mathies August 2017
-//when the page loads...
+/*---Built by Heather Mathies August 2017---*/
+
+/*---when the page loads...---*/
 $(document).ready(function() {
-    var questionsIndex = 0;
-    var question = 0;
-    var choices = $('#selections');
+
+   /*----------global variables---------*/
     var correct = 0;
     var incorrect = 0;
     var unanswered = 0;
     var i = 0;
-    var timeRemaining = 10;
+    var timeRemaining = 30;
     var timerId;
-    var giphy = ["assets/images/TNY-Livestream-Gif.gif", "assets/images/anigif_enhanced-5260-1406732708-8.gif", "assets/images/eclipse.transitGIF.gif", "assets/images/vF7MiG.gif"];
+    var giphy = ["assets/images/TNY-Livestream-Gif.gif", "assets/images/anigif_enhanced-5260-1406732708-8.gif",  "assets/images/vF7MiG.gif"];
     var j = 0;
-    // var length1 = allQuestions.length;
+
+    /*-----------------------these questions will be dyanimcally displayed one at a time----------------------------------------*/
     var allQuestions = [{
             q: "What year was the last solar eclipse?",
             a: ["1978", "1979", "1980", "1981"],
@@ -34,33 +35,37 @@ $(document).ready(function() {
             a: ["Sun Moon Earth", "Sun Earth Venus", "Earth Moon Stars", "Wind Earth Fire"],
             correct: 0
         }, {
-            q: "The solar eclipse totality last about..",
+            q: "The solar eclipse totality last almost..",
             a: ["60 minutes", "30 minutes", "3 minutes", "24 hours"],
             correct: 2
         },
-        // {
-        //     q:"Which lunar phase does a solar eclipse happen in?",
-        //     a:["Full Moon","Half Moon", "New Moon", "Crescent Moon"],
-        //     correct:2
-        // },
-        // {
-        //     q:"Which lunar phase does a solar eclipse happen in?",
-        //     a:["Full Moon","Half Moon", "New Moon", "Crescent Moon"],
-        //     correct:2
-        // },
+        {
+            q:"What year was the last total solar eclipse seen exclusively over the continental United States?",
+            a:["1980","1930", "1880", "1830"],
+            correct:2
+        },
+        {
+            q:"Totality is the only time when one can see the corona, which is the Sun's what?",
+            a:["photospere","atmosphere", "sunspots", "core"],
+            correct:1
+        },
     ];
 
-    /*when the page loads*/
+    /*------when the page first loads------*/
     $("#questionPage").hide();
     $("#resultsPage").hide();
-    /*when the start button is clicked*/
+
+
+    /*----when the start button is clicked--------*/
     $('.btn').on("click", function() {
         $("#startPage").hide();
         $("#questionPage").show();
         run();
     });
-    /*this function starts the timer and checks the value of the user answers*/
+
+    /*---this starts the timer, displays the questions with answers, and checks the value of the user answers-----*/
     function run() {
+            clearInterval(timerId);
             timerId = setInterval(decrement, 1000);
             // timeRemaining = 30;
             var correctAnswer = allQuestions[i].a[allQuestions[i].correct];
@@ -72,8 +77,6 @@ $(document).ready(function() {
                     html += '<button class="correct" type="button">' + value + '</button>';
                     $("#correct").html("<h3>" + "Correct: " + correct + "</h3>");
 
-
-                    console.log(correct);
                 } else {
                     html += '<button class="incorrect" type="button">' + value + '</button>';
                     $("#incorrect").html("<h3>" + "Incorrect: " + incorrect + "</h3>");
@@ -83,7 +86,9 @@ $(document).ready(function() {
             $('#question').html(html);
             i++;
         }
-        /*this function has the timer count down*/
+
+    /*--------------this has the timer count down and says what to do if the user doesn't answer before 
+    the timer runs out--------------------------------------*/
     function decrement() {
 
         timeRemaining--;
@@ -94,7 +99,6 @@ $(document).ready(function() {
             $("#giphyPage").show();
             $('#c-or-i').html("Times Up!");
             $('#giphyImage').html("<img src=" + giphy[0] + " width='350px'/>");
-            console.log(giphy[0]);
             clearInterval(timerId);
             setTimeout(nextQuestion, 1000 * 5);
             timeRemaining = 30;
@@ -103,57 +107,65 @@ $(document).ready(function() {
         }
     }
 
-    //use this to update the next question, add giphys, 
+    /*-----this onclick event detects the correct user answer----*/
     $("html").on("click", ".correct", function() {
+        
         correct++;
+        clearInterval(timerId);
         $('#questionPage').hide();
         $("#giphyPage").show();
         $('#c-or-i').html("Yup, that's correct!");
-        $('#giphyImage').html("<img src=" + giphy[3] + " width='350px'/>");
-        clearInterval(timerId);
+        $('#giphyImage').html("<img src=" + giphy[2] + " width='350px'/>");
         setTimeout(nextQuestion, 1000 * 5);
+        timeRemaining = 30;
     });
 
+    /*-----this onclick event detects the incorrect user answer----*/
     $("html").on("click", ".incorrect", function() {
+        
         incorrect++;
+        clearInterval(timerId);
         $('#questionPage').hide();
         $("#giphyPage").show();
         $('#c-or-i').html("Nope, that's incorrect!");
         $('#giphyImage').html("<img src=" + giphy[1] + " width='350px'/>");
-        clearInterval(timerId);
         setTimeout(nextQuestion, 1000 * 5);
-
-
+        timeRemaining = 30;
     });
 
+    /*------this displays the next question to the user------*/
     function nextQuestion() {
+        
         $('#startPage').hide();
         $("#resultsPage").hide();
         $("#giphyPage").hide();
         $('#questionPage').show();
         clearInterval(timerId);
         run();
-        if (allQuestions[i] === undefined) {
+        if (allQuestions[i] === null) {
             stop();
 
         }
     }
 
-    // this stopes the timer and displays the results
+   /*---------this stops the timer and displays the results----*/
     function stop() {
+        
         clearInterval(timerId);
         displayResults();
     }
 
-    // displaying the results to the screen
+  /*-------------displaying the results to the screen---------*/
     function displayResults() {
-
-            $("#resultsPage").show();
-            $('#questionPage').hide();
-            setTimeout(reset, 1000 * 10);
+            
+        $("#resultsPage").show();
+        $('#questionPage').hide();
+        setTimeout(reset, 1000 * 10);
         }
-    //this function resets the trivia game
+
+    /*-----------this function resets the trivia game----------*/
     function reset() {
+        
         $('#startPage').show();
         $("#resultsPage").hide();
         $("#questionPage").hide();
